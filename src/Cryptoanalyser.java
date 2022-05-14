d9935a8159b86491b2a511fc97d0e3c371e63880
Jamila Guliyeva <djamila.guliyeva>
2022-05-14 13:55:30 +0400

    all extra comments were deleted

--- src/Cryptoanalyser.java
+++ src/Cryptoanalyser.java
@@ -25,27 +25,7 @@ public class Cryptoanalyser {
             char[] buffer = new char[1024];
             while (reader.ready()) {
                 int real = reader.read(buffer);
-
-                String text = new String(buffer, 0, real);
-
                 Caesar.encrypt(symbols, key, buffer, real);
-//                for (int i = 0; i < real; i++) {
-//                    if (symbols.contains(buffer[i])) {
-//
-//                        int orderNumber = symbols.indexOf(buffer[i]);
-//                        int newOrderNumber = orderNumber + key;
-//
-//                        if (newOrderNumber < symbols.size()){
-//                            buffer[i] = symbols.get(newOrderNumber);
-//                        }
-//
-//                        if (newOrderNumber >= symbols.size()){
-//                            newOrderNumber = (newOrderNumber % symbols.size());
-//                            buffer[i] = symbols.get(newOrderNumber);
-//                        }
-//                    }
-//
-//                }
                 writer.write(buffer, 0, real);
             }
 
@@ -57,9 +37,7 @@ public class Cryptoanalyser {
         Scanner scanner1 = new Scanner(System.in);
         String option = scanner1.nextLine();
 
-
         if (option.equalsIgnoreCase("Да")) {
-
             System.out.println("Начинается процесс дешифрования");
             int keyReverse = 1;
 
@@ -72,51 +50,17 @@ public class Cryptoanalyser {
                 char[] buffer = new char[4096];
                 int real = 0;
                 while (reader.ready()) {
-
                     real = reader.read(buffer);
                 }
-                String textFromFile = new String(buffer, 0, real);
                 // начинаем работать с массивом, сожержащим зашифрованный текст
                 for (int j = 0; j < symbols.size(); j++) {
                     Caesar.decrypt(symbols, keyReverse, buffer, real);
-                    // присваиваем ключу значение j и перебираем все возможные вариианты
-//                for (int i = 0; i < real; i++) {
-//                    if (symbols.contains(buffer[i])) {
-//                        int orderNumber = symbols.indexOf(buffer[i]); // порядковый номер зашифрованной буквы
-//                        int newOrderNumber = orderNumber - keyReverse; // порядковый номер после дешифрования
-//
-//                        if ((newOrderNumber >= 0) && (newOrderNumber < symbols.size())) {
-//                            buffer[i] = symbols.get(newOrderNumber);
-//                        } else {
-//                            newOrderNumber = symbols.size() + newOrderNumber ;
-//                            buffer[i] = symbols.get(newOrderNumber);
-//                        }
-//                    }
-//
-//                }
-
                     // получили новый "расшифрованный массив символов"
-
                     boolean check = Caesar.findCorrectVersion(buffer);
-
                     if (check) {
                         writer.write(buffer, 0, real);
                         break;
                     }
-//                int count = 0;
-//                final int MAX = 35;
-
-//                for (int i = 0; i < real; i++) {
-//                    if (buffer[i] == ' ' ) // проходимся по массиву с целью выявить наличие пробелов " "
-//                    {
-//                        count = count + 1; // если пробелы есть то увеличиваем счетчик
-//                    }
-//
-//                    if (count > MAX){
-//                        writer.write(buffer,0, real); // если значение счетчика переваливает за 35, то записываем массив в файл
-//                        break;
-//                    }
-//                }
                 }
             } catch (IOException ex) {
                 System.out.println("File Reading/ File Writing Error");
